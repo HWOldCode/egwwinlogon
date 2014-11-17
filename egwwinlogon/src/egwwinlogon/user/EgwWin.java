79638621951fcb2e519b4df56c5478e9aff3d80a
KWA <katja.wagner@hw-softwareentwicklung.de>
2014-11-17 16:47:36 +0100

    update

--- egwwinlogon/src/egwwinlogon/user/EgwWin.java
+++ egwwinlogon/src/egwwinlogon/user/EgwWin.java
@@ -4,10 +4,37 @@
  */
 package egwwinlogon.user;
 
+import com.jegroupware.egroupware.Egroupware;
+import com.jegroupware.egroupware.EgroupwareBrowser;
+import com.jegroupware.egroupware.EgroupwareConfig;
+
 /**
  *
  * @author swe
  */
 public class EgwWin {
-	
+	/**
+	 * main
+	 * @param args String[]
+	 */
+	public static void main(String[] args) {
+            Egroupware egw = Egroupware.getInstance(new EgroupwareConfig(
+            "http://dev.hw-softwareentwicklung.de/egroupware/",
+            "default",
+            "admin2",
+            "test"
+            ));
+
+            try {
+                System.out.println(egw.getLoginDomains());
+                egw.login();
+                System.out.println(egw.getSession().getLastLoginId());
+                EgroupwareBrowser.open(egw);
+            }
+            catch( Exception e ) {
+                System.out.println(e.getMessage());
+            }
+
+            System.out.println("test");
+        }
 }
---
+++ egwwinlogon/src/egwwinlogon/user/Tray.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package egwwinlogon.user;
+
+import java.awt.* ;
+
+/**
+ *
+ * @author test
+ */
+public class Tray {
+    
+    private SystemTray _system_tray;
+}
