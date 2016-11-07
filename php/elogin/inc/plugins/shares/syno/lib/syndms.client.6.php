d4de63b52ab2cc7f4607a817ac77130a64801ec7
Stefan Werfling <stefan.werfling@hw-softwareentwicklung.de>
2016-11-07 15:09:07 +0100

    - Update dsm 6 api version 2

--- php/elogin/inc/plugins/shares/syno/lib/syndms.client.6.php
+++ php/elogin/inc/plugins/shares/syno/lib/syndms.client.6.php
@@ -79,4 +79,87 @@
 			$this->_initConnection = false;
             return false;
         }
+
+		/**
+		 * getFileSharesList
+		 * @param string $sharename
+		 * @param int $limit
+		 * @param array $options
+		 * @return array
+		 */
+        public function getFileSharesList($sharename, $limit=1000, $options=array()) {
+            if( $this->_isLogin ) {
+                $data = $this->_queryByService('SYNO.FileStation.List', array(
+                    'method'            => 'list',
+                    'version'           => '2',
+                    'folder_path'       => '"' . $this->_escapeFolderName($sharename) . '"',
+                    'filetype'          => '"all"',
+                    'additional'        => '["real_path","size","owner","time,perm","type","mount_point_type"]',
+                    'action'            => 'list',
+                    'sort_direction'    => '"ASC"',
+                    'sort_by'           => '"name"',
+                    'sort_by'           => 0,
+                    'limit'             => $limit,
+                    ), true);
+
+                if( $data && is_array($data) && isset($data['files']) ) {
+                    $files = (array) $data['files'];
+                    $rlist = array();
+
+                    foreach( $files as $file ) {
+                        $tfile = (array) $file;
+
+                        if( isset($options['only_dir']) && ($options['only_dir'] == true) ) {
+                            if( $tfile['isdir'] == true ) {
+                                $rlist[$tfile['path']] = $tfile['name'];
+                            }
+                        }
+                        elseif( isset($options['only_file']) && ($options['only_file'] == true) ) {
+                            if( $tfile['isdir'] == false ) {
+                                $rlist[$tfile['path']] = $tfile['name'];
+                            }
+                        }
+                        else {
+                            $rlist[$tfile['path']] = $tfile['name'];
+                        }
+                    }
+
+                    return $rlist;
+                }
+            }
+
+            return array();
+        }
+
+		/**
+         * createDirShare
+         * @param string $sharename
+         * @param string $dir
+         * @return boolean
+         */
+        public function createDirShare($sharename, $dir) {
+            if( $this->_isLogin ) {
+                $data = $this->_queryByService('SYNO.FileStation.CreateFolder', array(
+                    'method'            => 'create',
+                    'version'           => '2',
+                    'folder_path'       => '"' . $this->_escapeFolderName($sharename) . '"',
+                    'name'              => '"' . $this->_escapeFolderName($dir) . '"',
+                    'force_parent'      => true,
+                    ), true);
+
+                if( $data && is_array($data) && isset($data['folders']) ) {
+                    $folders = (array) $data['folders'];
+
+                    foreach( $folders as $tfolder ) {
+                        $folder = (array) $tfolder;
+
+                        if( $folder['name'] == $dir ) {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
 	}
